acb3a4b04c61529b17c570951fe019fc159cca79
omkar98 <edu.omkar@gmail.com>
2018-03-21 18:22:00 +0530

    Deleted IP.C

--- Numerical Methods and Scientific Computing/IP-OM.C
+++ Numerical Methods and Scientific Computing/IP-OM.C
@@ -1,3 +1,4 @@
+/*The problem is only with the count_get_data variable. Fix it appropriately.*/
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
@@ -12,27 +13,45 @@ static void force_fpf()
 struct array
 {
  int no;
- float *arr,*tarr;
+ float *arr;
  struct array *next;
 };
 struct array *start=NULL;
 int count_get_data=0;
 
+
 void get_data(int);
 void differ_values(int);
 void print_array(struct array*);
 
+
+
+
+struct array* create_array(int terms)
+{
+ struct array *temp=(struct array*)malloc(sizeof(struct array));
+ if(count_get_data<2)
+ {
+   temp->no=count_get_data;
+   temp->arr=(float*)malloc(sizeof(float)*terms);
+   temp->next=NULL;
+ }
+ else
+ {
+   temp->no=count_get_data;
+   temp->arr=(float*)malloc(sizeof(float)*terms-count_get_data+1);
+   temp->next=NULL;
+ }
+   return temp;
+}
+
 void get_data(int terms)
 {
-  int i;
-  struct array *temp1=start;
-  struct array *temp=(struct array*)malloc(sizeof(struct array));
-  temp->arr=(float*)malloc(sizeof(float)*terms);
-  temp->tarr=(float*)malloc(sizeof(float)*(terms-1));
-  temp->no=count_get_data;
-  /*If only one node exists*/
- // printf("\nStart: %d | Temp: %d", start, temp);
-  if(count_get_data==0)
+ int i;
+ struct array *temp1=start;
+ struct array *temp=create_array(terms);
+ /*If only one node exists:*/
+ if(count_get_data==0)
   {
    start=temp;
   }
@@ -47,47 +66,57 @@ void get_data(int terms)
     temp1->next=temp;
     //printf(" | Temp1->next = %d\n", temp1->next);
   }
-
-  temp->next=NULL;
-
   if(count_get_data==0 || count_get_data==1)
   {
     for(i=0; i<terms;i++)
     {
       scanf("%f", &temp->arr[i]);
     }
-
     for(i=0; i<terms;i++)
     {
       printf("\n%d = %.3f",&temp->arr[i], temp->arr[i]);
     }
   }
-  else
-  {
-    //differ_values(terms);
-  }
 
-  if(count_get_data!=0)
+  count_get_data++;
+  print_array(temp);
+
+  for(i=0;i<terms-1;i++)
   {
-    for(i=1;i<terms;i++)
+    if(temp->no>0)
     {
-      temp->tarr[i-1]=temp->arr[i]-temp->arr[i-1];
-    }
-    for(i=1;i<terms;i++)
-    {
-      printf("\ntarr[%d]=%f",i-1,temp->tarr[i-1]);
+      differ_values(terms);
     }
   }
+  //  count_get_data++;
+}
 
-  ++count_get_data;
+
+void differ_values(int terms)
+{
+  int i;
+  struct array *temp=create_array(terms);
+  struct array *temp1=start;
+
+  while(temp1->next!=NULL)
+    {
+     temp1=temp1->next;
+    }
+   // printf(" | Temp1 = %d", temp1);
+    temp1->next=temp;
+
+  for(i=0;i<terms-((temp1->no)-1);i++)
+  {
+   temp->arr[i]=(temp1->arr[i+1])-(temp1->arr[i]);
+  }
+
+  for(i=0;i<(terms-temp1->no);i++)
+  {
+   printf("\n%.3f", temp->arr[i]);
+  }
   print_array(temp);
 }
 
-/*differ_values(int terms)
-{
-
-
-} */
 void print_array(struct array *temp)
 {
  printf("\nArray Number: %d", temp->no);
